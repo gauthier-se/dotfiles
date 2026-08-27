1be690c0b2006f371578a8961a366d320b1fc745
gauthier-se <139963620+gauthier-se@users.noreply.github.com>
2026-08-27 20:22:07 +0200

    feat: auto-reload nvim buffers on external file changes

--- configs/nvim/.config/nvim/lua/autocmds.lua
+++ configs/nvim/.config/nvim/lua/autocmds.lua
@@ -8,12 +8,29 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Reload files when they change on disk
-vim.api.nvim_create_autocmd({ 'BufEnter', 'CursorHold', 'FocusGained', 'TermClose', 'TermLeave' }, {
+local autoread = vim.api.nvim_create_augroup('autoread', { clear = true })
+
+vim.api.nvim_create_autocmd({
+  'BufEnter',
+  'CursorHold',
+  'CursorHoldI',
+  'FocusGained',
+  'TermClose',
+  'TermLeave',
+}, {
   desc = 'Reload file when changed on disk',
-  group = vim.api.nvim_create_augroup('autoread', { clear = true }),
+  group = autoread,
   callback = function()
-    if vim.fn.mode() ~= 'c' then
+    if vim.fn.mode() ~= 'c' and vim.fn.getcmdwintype() == '' then
       vim.cmd 'checktime'
     end
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileChangedShellPost', {
+  desc = 'Tell me when a buffer was reloaded from disk',
+  group = autoread,
+  callback = function()
+    vim.notify('Buffer reloaded from disk', vim.log.levels.WARN)
   end,
 })
